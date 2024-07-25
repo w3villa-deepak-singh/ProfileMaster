75a0e30dff08286e7efdda4ae353e2d01db49f16
Deepak Singh <deepak.singh@w3villa.com>
2024-07-25 10:53:13 +0530

    isEmailVerification

---
+++ config/passport.js
@@ -0,0 +1,17 @@
+// const passport = require('passport');
+// const GoogleStrategy = require('passport-google-oauth20').Strategy;
+// const { UserProfile } = require('../models');
+
+// require('dotenv').config()
+
+// passport.use(new GoogleStrategy({
+//     clientID: process.env.GOOGLE_CLIENT_ID,
+//     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     callbackURL: "http://www.example.com/auth/google/callback"
+//   },
+//   function(accessToken, refreshToken, profile, cb) {
+//     User.findOrCreate({ googleId: profile.id }, function (err, user) {
+//       return cb(err, user);
+//     });
+//   }
+// ));
