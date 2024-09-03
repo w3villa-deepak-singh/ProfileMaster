4eff6bdd14b65bdc323167d35206a828c32b1d5c
Deepak Singh <deepak.singh@w3villa.com>
2024-09-03 10:24:25 +0530

    get course api

--- controllers/showCourses.js
+++ controllers/showCourses.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcrypt');
 const { v4: uuidv4 } = require('uuid');
-const { Courses } = require('../models');
-const {sendResponse} = require('../utils/responseHelper');
+const { Course } = require('../models');
+const { sendResponse } = require('../utils/responseHelper');
 
 
 
@@ -11,13 +11,54 @@ const {sendResponse} = require('../utils/responseHelper');
 const showAllCourse = async (req, res) => {
 
   try {
+    // Fetch all courses from the database
+    const courses = await Course.findAll();
 
-   
-    sendResponse(res, 200, 'show all courses');
-  
+    // Send the list of courses as a response
+    sendResponse(res, 200, 'Courses retrieved successfully', courses);
   } catch (error) {
+    console.error('Error fetching courses:', error);
+    sendResponse(res, 500, 'Internal Server Error');
+  }
+
+};
+
+
+const addCourse = async (req, res) => {
+  try {
+    const {
+      course_name,
+      description,
+      duration,
+      start_date,
+      end_date,
+      category,
+      price,
+      rating,
+      trainer,
+      oldprice,
+      rating_count,
+    } = req.body;
+
+    const newCourse = await Course.create({
+      course_name,
+      description,
+      duration,
+      start_date,
+      end_date,
+      category,
+      price,
+      rating,
+      trainer,
+      oldprice,
+      rating_count,
+    });
+
+    sendResponse(res, 200, 'Course added successfully', newCourse);
+  } catch (error) {
+    console.error('Error adding course:', error);
     sendResponse(res, 500, 'Internal Server Error');
   }
 };
 
-module.exports = { showAllCourse };
+module.exports = { showAllCourse, addCourse };
--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { showAllCourse } = require('../controllers/showCourses');
+const { showAllCourse,addCourse } = require('../controllers/showCourses');
 
 
 const router = express.Router();
 
 router.get('/showAllCourse', showAllCourse);
+router.post('/addCourse', addCourse);
 
 module.exports = router;
